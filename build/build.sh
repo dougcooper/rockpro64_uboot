7e68490bd4f958b0566f9fe5e8cf33d0a71d6904
dougcooper <douglas.cooper1@gmail.com>
2025-02-22 08:38:46 -0500

    update baud rate to 115200

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-
-git clone https://github.com/ARM-software/arm-trusted-firmware.git 
-git clone --depth 1 --branch v2022.07 https://source.denx.de/u-boot/u-boot.git
+#!/bin/bash -x
 
 cd /arm-trusted-firmware
-git checkout v2.12.0 && rm -f '*.bin'
 make -j$(nproc) PLAT=rk3399 "bl31"
 
 BL31="/arm-trusted-firmware/build/rk3399/release/bl31/bl31.elf"
@@ -15,6 +11,7 @@ make rockpro64-rk3399_defconfig
 make -j$(nproc) BL31="$BL31"
 ./tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl.bin:spl/u-boot-spl.bin idbloader-spi.img
 
-mkdir -p /output
-cp u-boot.itb /output
-cp idbloader-spi.img /output
+RELEASE_DIR="/src/release"
+mkdir -p $RELEASE_DIR
+cp u-boot.itb $RELEASE_DIR
+cp idbloader-spi.img $RELEASE_DIR
